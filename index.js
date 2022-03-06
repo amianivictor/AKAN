3777c34e610dcda8e7c1d1f0ded4c8b6babefd28
amianivictor <khahonjeri.amiani@student.moringaschool.com>
2022-03-06 22:53:14 +0300

    add js Business logic

---
+++ index.js
@@ -0,0 +1,7 @@
+//Business logic
+
+var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+var dateWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
