f2e4e0fbaf8f19c6e17497b536ded36ac0c67d70
amianivictor <khahonjeri.amiani@student.moringaschool.com>
2022-03-06 22:54:43 +0300

    add js user intaface

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 var dateWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+
+//userInterface
+
+function getInfo(){
+    var day = parseInt(document.getElementById("day").value);
+    
+    var month = parseInt(document.getElementById("month").value);
+    
+    var year = parseInt(document.getElementById("year").value);
+    
+    var gender = radioInfo();
